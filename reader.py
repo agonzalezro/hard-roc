f0a2a199513f6baebbb76f06244bce63641f2b5d
Álex González <agonzalezro@gmail.com>
2013-04-13 14:07:54 +0100

    First commit! :D

---
+++ reader.py
@@ -0,0 +1,17 @@
+import argparse
+import csv
+
+
+def process(file):
+    with open(file) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            print row
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some test data.')
+    parser.add_argument(
+        'file', metavar='file', type=str, help='the test CSV file'
+    )
+    args = parser.parse_args()
+    process(args.file)
