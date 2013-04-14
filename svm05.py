3596b9f3fcd96cfd1d6db38e6da326aa3fa058f1
lell <mathpunk@gmail.com>
2013-04-14 02:44:46 +0100

    .

--- svm05.py
+++ svm05.py
@@ -2,19 +2,22 @@ from sklearn import svm
 from numpy import arange
 from data import draft, proof, eval, save, normalize
 
-which = '_f05'
-(trX, trY, teX) = draft(which)
-#(trX, teX) = normalize(trX, teX)
+def dosvm(gamma = 1.0):
+  which = '_f05'
+  (trX, trY, teX) = draft(which)
+  (trX, teX) = normalize(trX, teX)
+  clf = svm.SVC(probability = True, kernel='rbf', gamma = gamma, tol=0.00001)
+  clf.fit(trX, trY)
+  teY = clf.predict_proba(teX)[:,1]
+  print eval(teY)
 
-clf = svm.NuSVC(probability=True, kernel='linear')
-clf.fit(trX, trY)
-teY = clf.predict_proba(teX)[:,1]
-print eval(teY)
+#for gamma in [0.001, 0.002, 0.004, 0.006, 0.008]:
+#  dosvm(gamma)
 
 #
-#(trX, trY, teX) = proof('_f04')
-#(trX, teX) = normalize(trX, teX)
-#clf = svm.NuSVC(probability=True, kernel='linear')
-#clf.fit(trX, trY)
-#teY = clf.predict_proba(teX)[:,1]
-#save(teY, 'pred_svm05.csv')
+(trX, trY, teX) = proof('_f05')
+(trX, teX) = normalize(trX, teX)
+clf = svm.SVC(probability=True, kernel='rbf', gamma=0.002, tol=0.00001)
+clf.fit(trX, trY)
+teY = clf.predict_proba(teX)[:,1]
+save(teY, 'pred_svm06.csv')
